74967dc1319fbf67896861e6b846637db389b8a6
guilhermesnts <guilhermess1994@gmail.com>
2020-05-05 10:47:00 -0300

    added encryption to localstorage and store only user and settings modules

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,22 @@ import createPersistedState from "vuex-persistedstate";
 import user from "./modules/user.js";
 import settings from "./modules/settings.js";
 import customers from "./modules/customers.js";
+import SecureLS from "secure-ls";
+var ls = new SecureLS({ isCompression: true });
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   mutations: {},
   actions: {},
   modules: { user, settings, customers },
-  plugins: [createPersistedState()]
+  plugins: [
+    createPersistedState({
+      paths: ["user", "settings"],
+      storage: {
+        getItem: key => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: key => ls.remove(key)
+      }
+    })
+  ]
 });
