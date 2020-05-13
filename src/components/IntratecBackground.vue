2c7c72293d187ff0047e4eae3da6bfe8157280c2
guilherme santos <guilhermess1994@gmail.com>
2020-05-13 16:11:32 -0300

    added background and card components

---
+++ src/components/IntratecBackground.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="background">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "intratecBackground"
+};
+</script>
+
+<style scoped>
+.background {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  vertical-align: middle;
+  justify-content: center;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.5)),
+    url("../assets/login_bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
---
+++ src/components/IntratecCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.card {
+  width: 400px;
+  min-height: 300px;
+  border-radius: 12px;
+  background: rgba(0, 0, 0, 0.6);
+  backdrop-filter: blur(8px);
+}
+</style>
