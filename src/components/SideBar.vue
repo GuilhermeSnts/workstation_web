f5d0d8ab23038fb1327f2ddfd4b7ff6d45d025f3
guilherme santos <guilhermess1994@gmail.com>
2020-06-06 00:17:28 -0300

    fixing sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,27 +1,23 @@
 <template>
-  <div class="sidebar">
-    <div class="middle">
-      <IntratecSidebarButton
-        @click="goto(item.route)"
-        v-for="(item, index) in menu"
-        :key="index"
-        :icon="item.icon"
-        :to="item.route"
-      />
-    </div>
-    <div class="bottom">
-      <IntratecSidebarButton icon="power" />
-    </div>
-  </div>
+  <v-navigation-drawer mini-variant app v-model="sideBar">
+    <v-list dense nav v-for="(item, index) in menu" :key="index">
+      <v-list-item :to="item.route">
+        <v-list-item-action>
+          <f-icon :icon="item.icon" />
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.text }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
 import { mapMutations, mapGetters } from "vuex";
-import IntratecSidebarButton from "@/components/IntratecSidebarButton";
+import FIcon from "@/components/FIcon";
 export default {
   data: () => ({
-    isActive: true,
-    windowSize: 0,
     menu: [
       { icon: "activity", text: "Dashboard", route: "/" },
       { icon: "users", text: "Customers", route: "/customers" },
@@ -38,27 +34,15 @@ export default {
     ]
   }),
   components: {
-    IntratecSidebarButton
+    FIcon
   },
 
   methods: {
-    ...mapMutations("settings", ["setSideBar"]),
-    resizeHandler() {
-      this.windowSize = document.documentElement.clientWidth;
-    }
+    ...mapMutations("settings", ["setSideBar"])
   },
 
   computed: {
     ...mapGetters("settings", ["getSideBar"]),
-
-    screensize() {
-      let screen = this.windowSize;
-      if (screen > 600) {
-        return "widescreen";
-      } else {
-        return "mobile";
-      }
-    },
 
     sideBar: {
       get() {
@@ -68,50 +52,6 @@ export default {
         this.setSideBar(value);
       }
     }
-  },
-  created() {
-    window.addEventListener("resize", this.resizeHandler());
   }
-  // destroyed() {
-  //   window.removeEventListener("resize");
-  // }
 };
 </script>
-
-<style lang="sass" scoped>
-@mixin flexColumn
-  display: flex
-  flex-direction: column
-  justify-content: center
-  vertical-align: middle
-  align-items: center
-
-@mixin flexRow
-  display: flex
-  flex-direction: row
-
-@media (max-width: 600px)
-  .sidebar
-    background: red
-    width: 100vh
-    height: 60px
-    @include flexRow
-    .middle
-      @include flexRow
-
-@media (min-width: 600px)
-  .sidebar
-    background: blue
-    width: 60px
-    height: 100vh
-    @include flexColumn
-    .middle
-      @include flexColumn
-
-.sidebar
-  position: fixed
-  z-index: 99
-  background: rgba(0,0,0,0.4)
-  top: 0
-  left: 0
-</style>
